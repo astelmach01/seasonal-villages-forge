2984d27d28e6ea571b000d83d99d1e23702c1467
astelmach01 <58709763+astelmach01@users.noreply.github.com>
2026-05-09 15:00:16 -0400

    Add CI and release workflow

--- build-local.bat
+++ build-local.bat
@@ -3,5 +3,6 @@ setlocal
 set "ROOT=%~dp0"
 set "JAVA_HOME=%ROOT%..\\seasonal-villages-fabric\\.tools\\jdk-21"
 set "PATH=%JAVA_HOME%\bin;%PATH%"
+cd /d "%ROOT%"
 call "%ROOT%gradlew.bat" %*
 exit /b %ERRORLEVEL%
